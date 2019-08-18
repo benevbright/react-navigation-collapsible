242a24da17797e9661d166696e94cd64a237cb9b
Bright Lee <benevbright@gmail.com>
2019-08-18 22:46:46 +0200

    lint example

--- example/src/S0_DefaultHeader.js
+++ example/src/S0_DefaultHeader.js
@@ -1,63 +1,69 @@
 import React, {Component} from 'react';
-import { Text, FlatList, Animated, TouchableOpacity } from 'react-native';
+import {Text, FlatList, Animated, TouchableOpacity} from 'react-native';
 
-import { withCollapsible } from 'react-navigation-collapsible';
+import {withCollapsible} from 'react-navigation-collapsible';
 
 const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
-class DefaultHeaderScreen extends Component{
+class DefaultHeaderScreen extends Component {
   static navigationOptions = {
-    title: 'Default Header'
+    title: 'Default Header',
   };
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     const data = [];
-    for(let i = 0 ; i < 60 ; i++){
+    for (let i = 0; i < 60; i++) {
       data.push(i);
     }
 
     this.state = {
-      data: data
-    }
+      data: data,
+    };
   }
 
   renderItem = ({item}) => (
-    <TouchableOpacity 
+    <TouchableOpacity
       onPress={() => {
         this.props.navigation.navigate('DetailScreen');
       }}
-      style={{width: '100%', height: 50, borderBottomColor: '#0002', borderBottomWidth: 0.5, paddingHorizontal: 20, justifyContent: 'center'}}>
+      style={{
+        width: '100%',
+        height: 50,
+        borderBottomColor: '#0002',
+        borderBottomWidth: 0.5,
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+      }}>
       <Text style={{fontSize: 22}}>{item}</Text>
     </TouchableOpacity>
-  )
+  );
 
   // onScroll = e => {
   //   console.log(e.nativeEvent.contentOffset);
   // }
 
-  render(){
-    const { paddingHeight, animatedY, onScroll } = this.props.collapsible;
+  render() {
+    const {paddingHeight, animatedY, onScroll} = this.props.collapsible;
 
     return (
-      <AnimatedFlatList 
+      <AnimatedFlatList
         style={{flex: 1}}
         data={this.state.data}
         renderItem={this.renderItem}
         keyExtractor={(item, index) => String(index)}
-
         contentContainerStyle={{paddingTop: paddingHeight}}
         scrollIndicatorInsets={{top: paddingHeight}}
-        onScroll={onScroll} 
+        onScroll={onScroll}
         // if you want to use 'onScroll' callback.
         // onScroll={Animated.event(
         //   [{nativeEvent: {contentOffset: {y: animatedY}}}],
-        //   {useNativeDriver:true, listener:this.onScroll})} 
+        //   {useNativeDriver:true, listener:this.onScroll})}
         _mustAddThis={animatedY}
-        />
-    )
+      />
+    );
   }
 }
 
-export default withCollapsible(DefaultHeaderScreen, { iOSCollapsedColor: 'red' });
+export default withCollapsible(DefaultHeaderScreen, {iOSCollapsedColor: 'red'});
